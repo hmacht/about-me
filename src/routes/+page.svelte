5a1ae1a6772d685caecee5f9d7be8238980b4b0b
hmacht <32553134+hmacht@users.noreply.github.com>
2024-07-21 14:49:57 -0400

    main page added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+  .main-container {
+    padding: 5rem;
+  }
+
+  @media (max-width: 768px) {
+    .main-container {
+      padding: 10px;
+    }
+  }
+
+  .title {
+    font-weight: 800;
+    font-size: 24px;
+  }
+
+  a {
+    color: black;
+    text-decoration: underline;
+  }
+</style>
+
+<div class="main-container">
+  <p class="title">Hi, I'm Henry Macht</p>
+  <p>I graduated from Virginia Tech in 2023 with a bachelor’s degree in Computer Science. I started programming in 2012 and never looked back. P.S. I have decided to make this site as simple as possible to focus more time on my projects.</p>
+
+  <b>Macht Development</b>
+  <p>Our mission is to build platforms with sustainable online behavior. We built FAADE in 2020 to create a healthier space for social media, and we built True Capture in 2024 to help internet users decipher what content is real online. I have built a team of three top-of-their-class engineers (including myself), as well as a few business-savvy friends and, of course, my girlfriend who runs all our marketing. Oh, and I have created some popular word games. Here is a complete list of our work:</p>
+  <ul>
+    <li><a href="https://apps.apple.com/us/app/faade/id1580493013"><img src="/faade.png" width="15px" alt="FAADe"> FAADE</a></li>
+    <li><a href="https://apps.apple.com/us/app/true-capture-verified-moments/id6471775986"><img src="/true-capture.png" width="15px" alt="True Capture"> True Capture: Verified Moments</a></li>
+    <li><a href="https://apps.apple.com/us/app/fliptype/id1527890657"><img src="/fliptype.png" width="15px" alt="FlipType"> FlipType</a> (independent project) with over 10,000 players</li>
+    <li><a href="https://theletterloop.com"><img src="/theletterloop.png" width="15px" alt="The LetterLoop"> The LetterLoop</a> (independent project) with over 30,000 players</li>
+  </ul>
+
+  <br>
+  <b>Work Experience</b>
+  <p>Graduated from Virginia Tech in 2023. After graduation, I started working full-time at BLADE as a software engineer. BLADE is a Ruby shop and has two primary products: a customer-facing booking site and an internal flight logistics and reservations system.</p>
+  <ul>
+    <li><a href="https://www.draper.com"><img src="/draper.png" width="15px" alt="Draper Laboratory"> Draper Laboratory</a> Software Engineer Intern (summer 2018)</li>
+    <li><a href="https://www.haventech.us"><img src="/haven-tech.png" width="15px" alt="Haven Technologies"> Haven Technologies</a> Software Engineer Intern (summer 2020)</li>
+    <li><a href="https://www.blade.com"><img src="/blade.png" width="15px" alt="BLADE"> BLADE</a> Full-Time Software Engineer (2023-present)</li>
+  </ul>
+
+  <br>
+  <b>Contact Me</b>
+  <ul>
+    <li><a href="https://github.com/hmacht">Github</a></li>
+    <li><a href="https://x.com/hmacht19">Find me on 𝕏</a></li>
+    <li><a href="mailto:henryamacht@gmail.com">henryamacht@gmail.com</a></li>
+  </ul>
+</div>
